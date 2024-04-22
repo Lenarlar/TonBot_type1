d62b9ded960cb31871b657c777b132026d6596f2
Lenarlar <133435171+Lenarlar@users.noreply.github.com>
2024-04-22 19:05:12 +0300

    Update index.js
    reparate modules

--- index.js
+++ index.js
@@ -6,24 +6,6 @@ const bot = new TelegramApi(token, {polling: true})
 
 const chats = {}
 
-const gameOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
-            [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
-            [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
-            [{text: '0', callback_data: '0'}], [{text: 'End', callback_data: 'back'}]
-        ]
-    })
-}
-
-const againOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{text: 'Новая игра', callback_data: '/again'}]
-        ]
-    })
-}
 
 const startGame = async (chatId) => {
     await bot.sendMessage(chatId, `Число от 1 до 9`)
